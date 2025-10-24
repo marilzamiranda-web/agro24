44bd9ad45ce68fe4d3cb00e396ac7d044a4249e4
Marilza Miranda <marilza.miranda@aluno.senai.br>
2025-10-23 21:25:41 -0400

    Criação da tela de login

---
+++ app/login.tsx
@@ -0,0 +1,21 @@
+import {View, Text, StyleSheet} from 'react-native';
+export default function Login () {
+    return (
+        <View style={styles.container}>
+            <Text>
+                So alegria!
+            < /Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    }
+});
+
+
+    
+
+
+
